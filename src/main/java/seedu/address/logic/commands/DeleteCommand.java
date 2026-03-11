d031598397744e40a688d1a2b37f32c2b2b6ac0b
Foysal's Macbook <e1333986@u.nus.edu>
2026-03-12 00:19:20 +0800

    Add support for multiple deletes

--- src/main/java/seedu/address/logic/commands/DeleteCommand.java
+++ src/main/java/seedu/address/logic/commands/DeleteCommand.java
@@ -2,7 +2,11 @@ package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import seedu.address.commons.core.index.Index;
 import seedu.address.commons.util.ToStringBuilder;
@@ -19,30 +23,56 @@ public class DeleteCommand extends Command {
     public static final String COMMAND_WORD = "delete";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD
-            + ": Deletes the location identified by the index number used in the displayed location list.\n"
-            + "Parameters: INDEX (must be a positive integer)\n"
-            + "Example: " + COMMAND_WORD + " 1";
+            + ": Deletes the location(s) identified by the index number(s) used in the displayed location list.\n"
+            + "Parameters: INDEX [MORE_INDEXES]... (indices must be positive integers)\n"
+            + "Example: " + COMMAND_WORD + " 1 2 3";
 
     public static final String MESSAGE_DELETE_LOCATION_SUCCESS = "Deleted Location: %1$s";
+    public static final String MESSAGE_DELETE_LOCATIONS_SUCCESS = "Deleted Locations:\n%1$s";
+    public static final String MESSAGE_DUPLICATE_INDEX = "Duplicate indices are not allowed.";
 
-    private final Index targetIndex;
+    private final List<Index> targetIndexes;
 
-    public DeleteCommand(Index targetIndex) {
-        this.targetIndex = targetIndex;
+    public DeleteCommand(List<Index> targetIndexes) {
+        requireNonNull(targetIndexes);
+        if (targetIndexes.isEmpty()) {
+            throw new IllegalArgumentException("At least one index must be provided.");
+        }
+        this.targetIndexes = List.copyOf(targetIndexes);
     }
 
     @Override
     public CommandResult execute(Model model) throws CommandException {
         requireNonNull(model);
         List<Location> lastShownList = model.getFilteredLocationList();
+        Set<Integer> uniqueIndexes = new HashSet<>();
+        List<Location> locationsToDelete = new ArrayList<>();
 
-        if (targetIndex.getZeroBased() >= lastShownList.size()) {
-            throw new CommandException(Messages.MESSAGE_INVALID_LOCATION_DISPLAYED_INDEX);
+        for (Index targetIndex : targetIndexes) {
+            if (!uniqueIndexes.add(targetIndex.getZeroBased())) {
+                throw new CommandException(MESSAGE_DUPLICATE_INDEX);
+            }
+
+            if (targetIndex.getZeroBased() >= lastShownList.size()) {
+                throw new CommandException(Messages.MESSAGE_INVALID_LOCATION_DISPLAYED_INDEX);
+            }
+
+            locationsToDelete.add(lastShownList.get(targetIndex.getZeroBased()));
         }
 
-        Location locationToDelete = lastShownList.get(targetIndex.getZeroBased());
-        model.deleteLocation(locationToDelete);
-        return new CommandResult(String.format(MESSAGE_DELETE_LOCATION_SUCCESS, Messages.format(locationToDelete)));
+        for (Location locationToDelete : locationsToDelete) {
+            model.deleteLocation(locationToDelete);
+        }
+
+        if (locationsToDelete.size() == 1) {
+            return new CommandResult(String.format(MESSAGE_DELETE_LOCATION_SUCCESS,
+                    Messages.format(locationsToDelete.get(0))));
+        }
+
+        String deletedLocations = locationsToDelete.stream()
+                .map(Messages::format)
+                .collect(Collectors.joining("\n"));
+        return new CommandResult(String.format(MESSAGE_DELETE_LOCATIONS_SUCCESS, deletedLocations));
     }
 
     @Override
@@ -57,13 +87,13 @@ public class DeleteCommand extends Command {
         }
 
         DeleteCommand otherDeleteCommand = (DeleteCommand) other;
-        return targetIndex.equals(otherDeleteCommand.targetIndex);
+        return targetIndexes.equals(otherDeleteCommand.targetIndexes);
     }
 
     @Override
     public String toString() {
         return new ToStringBuilder(this)
-                .add("targetIndex", targetIndex)
+                .add("targetIndexes", targetIndexes)
                 .toString();
     }
 }
--- src/main/java/seedu/address/logic/parser/DeleteCommandParser.java
+++ src/main/java/seedu/address/logic/parser/DeleteCommandParser.java
@@ -2,6 +2,9 @@ package seedu.address.logic.parser;
 
 import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.DeleteCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
@@ -18,8 +21,15 @@ public class DeleteCommandParser implements Parser<DeleteCommand> {
      */
     public DeleteCommand parse(String args) throws ParseException {
         try {
-            Index index = ParserUtil.parseIndex(args);
-            return new DeleteCommand(index);
+            String trimmedArgs = args.trim();
+            String[] indexTokens = trimmedArgs.split("\\s+");
+
+            List<Index> indexes = new ArrayList<>();
+            for (String indexToken : indexTokens) {
+                indexes.add(ParserUtil.parseIndex(indexToken));
+            }
+
+            return new DeleteCommand(indexes);
         } catch (ParseException pe) {
             throw new ParseException(
                     String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteCommand.MESSAGE_USAGE), pe);
